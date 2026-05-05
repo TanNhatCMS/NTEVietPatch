2f9ac4ad2d4b39640df09bc0872df71fe9fc0e42
KietNT <113796420+TanNhatCMS@users.noreply.github.com>
2026-05-05 13:49:02 +0700

    ✨ Cập nhật APP_VERSION "1.0.0" → "1.0.1".

--- config.py
+++ config.py
@@ -1,15 +1,6 @@
 # config.py
 
 # --- App Version ---
-APP_VERSION = "1.0.0"
-
-# --- Repositories ---
-# Repo chứa các file mod (Việt Hóa)
-GITHUB_MOD_REPO = "CallMeDangDev/NTE-Viet-Hoa"
-GITHUB_MOD_API = "https://api.github.com/repos/CallMeDangDev/NTE-Viet-Hoa/releases"
-
-# Repo chứa bản cập nhật của chính phần mềm (Tool EXE)
-GITHUB_APP_REPO = "TanNhatCMS/NTEVietPatch"
-GITHUB_APP_API = "https://api.github.com/repos/TanNhatCMS/NTEVietPatch/releases/latest"
+APP_VERSION = "1.0.1"
 
 
